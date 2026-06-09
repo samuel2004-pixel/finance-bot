45b6103b80c6e975ea8297b22e7a9d233be73f5d
Samuel Roshan <samuelroshanin@gmail.com>
2026-06-09 23:30:33 +0530

    Switch to psycopg3

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg.connect(DATABASE_URL)
 
 
 def init_db():
